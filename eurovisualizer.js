e4dce83ad9d3c7416b3513a1adb7b92268d12a82
Jakob Kallin <jakobkallin@gmail.com>
2013-05-26 23:12:42 +0200

    Folder structure

--- eurovisualizer.js
+++
@@ -1,9 +0,0 @@
-angular
-.module('eurovisualizer', [])
-.directive('europe', function() {
-	return {
-		restrict: 'E',
-		templateUrl: 'europe.svg',
-		replace: true
-	}
-});
---
+++ source/eurovisualizer.js
@@ -0,0 +1,30 @@
+angular
+.module('eurovisualizer', [])
+.directive('europe', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'source/europe.svg',
+		replace: true,
+		scope: {
+			classes: '=classes'
+		},
+		compile: function($svg, attrs) {
+			var svg = $svg[0];
+			var paths = svg.getElementsByTagName('path');
+			Array.prototype.forEach.call(paths, function(path) {
+				path.dataset.ngClass = 'classes.' + path.id;
+				
+				// Enable binding to SVG class.
+				Object.defineProperty(path, 'className', {
+					get: function() {
+						// JQLite expects empty string if no class, otherwise it's converted to the string "null".
+						return this.getAttribute('class') || '';
+					},
+					set: function(value) {
+						this.setAttribute('class', value);
+					}
+				});
+			});
+		}
+	};
+});
